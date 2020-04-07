d430dfd9dd6d7983c9884f37658bf2f95ee8cf2a
Spencer Pearson <spencerpearson@dropbox.com>
2020-04-07 11:31:00 -0700

    just print pings; and add --start-seconds-ago

--- stagtime/__main__.py
+++ stagtime/__main__.py
@@ -1,42 +1,54 @@
 import argparse
 import datetime
+import enum
 import json
-from pathlib import Path
-import subprocess
-import threading
+import sys
 import time
 import typing as t
 from stagtime import Timekeeper
 
+UTC = datetime.timezone.utc
+
+class PingFormat(enum.Enum):
+    JSON = 'json'
+    ISO8601 = 'iso8601'
+    LOCAL = 'local'
+
+FORMAT_FUNCS = {
+    PingFormat.ISO8601: lambda now, nextping: nextping.astimezone(UTC).strftime('%Y-%m-%dT%H:%M:%SZ'),
+    PingFormat.LOCAL: lambda now, nextping: nextping.astimezone(None).strftime('%Y-%m-%d %H:%M:%S'),
+    PingFormat.JSON: lambda now, nextping: json.dumps({
+        PingFormat.ISO8601.value: FORMAT_FUNCS[PingFormat.ISO8601](now, nextping),
+        PingFormat.LOCAL.value: FORMAT_FUNCS[PingFormat.LOCAL](now, nextping),
+    }),
+}
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--logfile", type=Path, default=Path.home()/'.tagtime.jsons')
-parser.add_argument("--on-ping-cmd", default=None, type=str)
+parser.add_argument("--format", choices=list(PingFormat), type=PingFormat, default=PingFormat.JSON)
+parser.add_argument("--start-seconds-ago", type=float, default=0.0)
 
 def main(
+    ping_format: PingFormat,
     timekeeper: Timekeeper = None,
-    get_time: t.Callable[[], datetime.datetime] = datetime.datetime.now,
+    get_time: t.Callable[[], datetime.datetime] = lambda: datetime.datetime.now(UTC),
     sleep: t.Callable[[datetime.timedelta], None] = lambda dt: time.sleep(dt.total_seconds()),
+    print: t.Callable[[str], None] = print,
+    start_ago: datetime.timedelta = datetime.timedelta(seconds=0),
 ):
     if timekeeper is None:
         timekeeper = Timekeeper()
 
-    timekeeper.init(get_time().timestamp())
+    timekeeper.init((get_time() - start_ago).timestamp())
     while True:
-        nextping = datetime.datetime.fromtimestamp(timekeeper.nextping())
+        nextping = datetime.datetime.fromtimestamp(timekeeper.nextping(), tz=UTC)
         now = get_time()
         if now < nextping:
             sleep(nextping - get_time())
-        if args.on_ping_cmd is not None:
-            threading.Thread(target=lambda: subprocess.run(args.on_ping_cmd, shell=True)).start()
-        try:
-            response = input("Whatcha doing ({:%Y-%m-%d %H:%M:%S})? ".format(nextping))
-        except KeyboardInterrupt:
-            print("Keyboard interrupt caught; exiting")
-            return
-        with args.logfile.open('a') as f:
-            f.write(json.dumps({"ts": nextping.strftime("%Y-%m-%dT%H:%M:%SZ"), "response": response}))
-            f.write('\n')
+        print(FORMAT_FUNCS[args.format](now, nextping))
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main()
+    try:
+        main(ping_format=args.format, start_ago=datetime.timedelta(seconds=args.start_seconds_ago))
+    except KeyboardInterrupt:
+        print("Keyboard interrupt caught, exiting...", file=sys.stderr)
